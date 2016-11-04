455ef69f5178f12c94fe165fac26b9662a608f77
jk <vargsmane@gmx.de>
2016-11-04 13:54:08 +0100

    + First attempts at 159

---
+++ 159.py
@@ -0,0 +1,42 @@
+import hashlib
+import binascii
+
+import itertools
+
+from timer import Timer
+from parser import Parser
+import string
+
+crack = Timer("cracking")
+sendtime = Timer("sending")
+
+parser = Parser(159)
+parser.set_cookie('fdlhp5k1oir35tp771tfuou527')
+parser.connect()
+parser.toggle_debug()
+
+messages = parser.get_messages()
+hash_value = messages[0]
+
+crack.start_timer()
+generator = itertools.product(string.ascii_lowercase + '0123456789', repeat=6)
+
+attempt = None
+while True:
+    try:
+        attempt = ''.join(generator.__next__())
+        cipher = hashlib.sha1()
+        cipher.update(attempt.encode())
+        if binascii.hexlify(cipher.digest()).decode() == hash_value:
+            print("Found the message: %s" % attempt)
+            break
+    except StopIteration:
+        exit("Value not in this generator")
+crack.stop_timer()
+
+sendtime.start_timer()
+parser.send_solution(attempt)
+sendtime.stop_timer()
+
+crack.print()
+sendtime.print()
